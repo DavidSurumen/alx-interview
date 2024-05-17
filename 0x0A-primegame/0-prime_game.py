83dfb2984711b6f08b4a003eb6974ad052fa8391
David Surumen <davidsurumen@gmail.com>
2024-05-17 03:43:57 +0300

    task 1 -prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Game of prime numbers. Contains one function -isWinner
+"""
+def isWinner(x, nums):
+    """
+    Returns the winner of the game, where two players take turns finding a prime number.
+    """
+    ben = 0
+    maria = 0
+    for n in nums:
+        prime = [True for i in range(n+1)]
+        p = 2
+        while(p*p <= n):
+            if prime[p]:
+                for i in range(p*p, n+1, p):
+                    prime[i] = False
+            p += 1
+        primes = []
+        for p in range(2, n+1):
+            if prime[p]:
+                primes.append(p)
+        if len(primes) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
+    if ben > maria:
+        return "Ben"
+    elif maria > ben:
+        return "Maria"
+    else:
+        return None
